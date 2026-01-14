52d9ef2eb29190839f383d2145407ada3fbbdfa9
rares <rares21m@gmail.com>
2026-01-14 13:01:44 +0200

    config: URL API dinamic pentru deploy

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 
 // configuram axios cu URL-ul backendului
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
     headers: {
         'Content-Type': 'application/json',
     },
